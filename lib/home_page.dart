864cbc40098c16b0b317082b624c6efa9559ba08
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2020-03-27 22:43:00 +0530

    home_page: Add list of clickable items.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,11 +29,39 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: new Center(
-        child: new Text(
-          'Welcome',
-          style: new TextStyle(fontSize: 32.0),
-        ),
-      )
+          child: _buildList()),
     );
+
+
   }
+
+  Widget _buildList() => ListView(
+    children: [
+      _tile('Donation', 'Donate Some Money', Icons.account_balance),
+      Divider(),
+      _tile('Flag yourself', 'as a victim of COVID-19', Icons.accessibility_new),
+      Divider(),
+      _tile('Cook Food', 'yummy food!!!', Icons.fastfood),
+      Divider(),
+      _tile('Deliver Food', 'You must have a vehicle', Icons.directions_bike),
+      Divider(),
+    ],
+  );
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+    title: Text(title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+    onTap: () {
+      
+    },
+  );
+
 }
